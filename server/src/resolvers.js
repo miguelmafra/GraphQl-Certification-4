fb59ea65e8d1e1943b7cef81b4d50423e2f6eb54
Miguel Lorenzi <miguelml159357@gmail.com>
2023-04-28 10:58:20 -0300

    Try catch adicionado aos resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -23,6 +23,30 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    
+  },
+
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
+    },
   },
 };
 
